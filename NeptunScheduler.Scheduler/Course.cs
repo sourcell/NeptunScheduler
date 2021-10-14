b2b3a2184c9117cb3e35d165e0a0c5e7147cec84
Marcell Sárkány <marci.sarkany@gmail.com>
2021-10-14 20:08:41 +0200

    Fix spaces

--- NeptunScheduler.Scheduler/Course.cs
+++ NeptunScheduler.Scheduler/Course.cs
@@ -3,9 +3,9 @@ namespace NeptunScheduler.Scheduler
     class Course : TimeBlock
     {
         public string SubjectName { get; set; }
-		public string ID { get; set; }
-		public int AvailableSlots { get; set; }
-		public int Day { get; set; }
-		public bool Fix { get; set; }
+        public string ID { get; set; }
+        public int AvailableSlots { get; set; }
+        public int Day { get; set; }
+        public bool Fix { get; set; }
     }
 }
