4131240167980a50b47296702f5b703d289a7c27
Marcell Sárkány <marci.sarkany@gmail.com>
2021-10-14 18:39:11 +0200

    Add console app

---
+++ NeptunScheduler.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NeptunScheduler.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
