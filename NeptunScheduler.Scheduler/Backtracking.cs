f3eb9467d3974a158083243975681aa10c7e5af7
Marcell Sárkány <marci.sarkany@gmail.com>
2021-10-15 15:42:42 +0200

    Add backtracking skeleton

---
+++ NeptunScheduler.Scheduler/Backtracking.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NeptunScheduler.Scheduler
+{
+    public class Backtracking
+    {
+        public List<List<Course>> PossibleResults(List<Subject> subjects, List<BusyTimeBlock> busies)
+        {
+            // This list will contain all the possible schedules
+            List<List<Course>> finalResults = new List<List<Course>>();
+
+            // Accumulate all the fix courses and busy timeblocks into this list
+            List<TimeBlock> fixes = new List<TimeBlock>();
+
+            subjects.ForEach(s => {
+                s.Courses.ForEach(c => {
+                    if (c.Fix)
+                    {
+                        fixes.Add(c);
+                    }
+                });
+            });
+
+            busies.ForEach(b => fixes.Add(b));
+
+            // Check if there is collision between fix timeblocks
+            if (CheckCollision(fixes))
+            {
+                throw new ArgumentException("There are collisions.");
+            }
+
+            // Temporary result to work with
+            Course[] result = new Course[subjects.Count];
+
+            Backtrack(subjects, fixes, result, 0, finalResults);
+
+            return finalResults;
+        }
+
+        private bool CheckCollision(List<TimeBlock> timeBlocks)
+        {
+            int collisions = 0;
+
+            timeBlocks.ForEach(x => {
+                timeBlocks.ForEach(y => {
+                    if (x != y && x.CollideWith(y))
+                    {
+                        collisions++;
+                    }
+                });
+            });
+
+            return collisions / 2 > 0;
+        }
+
+        private void Backtrack(List<Subject> subjects, List<TimeBlock> fixes, Course[] result, int level, List<List<Course>> finalResults)
+        {
+            // Optional Courses of the current Subject
+            List<Course> courses = subjects[level].Courses.Where(c => !c.Fix).ToList();
+            
+            for (int i = 0; i < courses.Count; i++)
+            {
+                result[level] = courses[i];
+
+                if (level == result.Length - 1)
+                {
+                    finalResults.Add(result.ToList());
+                }
+                else
+                {
+                    Backtrack(subjects, fixes, result, level + 1, finalResults);
+                }
+            }
+        }
+    }
+}
