38e39bdb97a081c6f63073fd1a742dc052c8e9db
Marcell Sárkány <marci.sarkany@gmail.com>
2021-10-15 11:25:17 +0200

    Use list instead of array

--- NeptunScheduler.Scheduler/Subject.cs
+++ NeptunScheduler.Scheduler/Subject.cs
@@ -1,8 +1,10 @@
+using System.Collections.Generic;
+
 namespace NeptunScheduler.Scheduler
 {
     public class Subject
     {
         public string Title { get; set; }
-        public Course[] Courses { get; set; }
+        public List<Course> Courses { get; set; }
     }
 }
