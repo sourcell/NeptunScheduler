fbaf24e70e2ceeb7766716cd867e1a3056463b5f
Marcell Sárkány <marci.sarkany@gmail.com>
2021-12-09 17:36:43 +0100

    Modify Course class

--- NeptunScheduler.ConsoleApp/Program.cs
+++ NeptunScheduler.ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using NeptunScheduler.Scheduler;
 
 namespace NeptunScheduler.ConsoleApp
 {
@@ -6,7 +8,38 @@ namespace NeptunScheduler.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Course> courses1 = new List<Course>()
+            {
+                new Course() { SubjectName = "Tárgy #1", Day = 1, Fix = true, Start = new Time(8, 0), End = new Time(9, 30) },
+                new Course() { SubjectName = "Tárgy #1", Day = 2, Fix = false, Start = new Time(8, 0), End = new Time(9, 30) },
+                new Course() { SubjectName = "Tárgy #1", Day = 3, Fix = false, Start = new Time(8, 0), End = new Time(9, 30) }
+            };
+            Subject subject1 = new Subject() { Title = "Tárgy #1", Courses = courses1 };
+
+            List<Course> courses2 = new List<Course>()
+            {
+                new Course() { SubjectName = "Tárgy #2", Day = 1, Fix = true, Start = new Time(10, 0), End = new Time(11, 30) },
+                new Course() { SubjectName = "Tárgy #2", Day = 2, Fix = false, Start = new Time(10, 0), End = new Time(11, 30) },
+                new Course() { SubjectName = "Tárgy #2", Day = 3, Fix = false, Start = new Time(10, 0), End = new Time(11, 30) }
+            };
+            Subject subject2 = new Subject() { Title = "Tárgy #2", Courses = courses2 };
+
+            List<Course> courses3 = new List<Course>()
+            {
+                new Course() { SubjectName = "Tárgy #3", Day = 1, Fix = true, Start = new Time(12, 0), End = new Time(13, 30) },
+                new Course() { SubjectName = "Tárgy #3", Day = 2, Fix = false, Start = new Time(12, 0), End = new Time(13, 30) },
+                new Course() { SubjectName = "Tárgy #3", Day = 3, Fix = false, Start = new Time(12, 0), End = new Time(13, 30) }
+            };
+            Subject subject3 = new Subject() { Title = "Tárgy #3", Courses = courses3 };
+
+            List<Subject> subjects = new List<Subject>();
+            subjects.Add(subject1);
+            subjects.Add(subject2);
+            subjects.Add(subject3);
+
+            Backtracking bt = new Backtracking();
+
+            bt.PossibleResults(subjects, new List<BusyTimeBlock>());
         }
     }
 }
--- NeptunScheduler.Scheduler/Course.cs
+++ NeptunScheduler.Scheduler/Course.cs
@@ -3,8 +3,11 @@ namespace NeptunScheduler.Scheduler
     public class Course : TimeBlock
     {
         public string SubjectName { get; set; }
-        public string ID { get; set; }
+        public string Id { get; set; }
         public int AvailableSlots { get; set; }
         public bool Fix { get; set; }
+        public bool CanCollide { get; set; }
+        public int Priority { get; set; }
+        public bool Ignored { get; set; }
     }
 }
