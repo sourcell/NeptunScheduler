479e66d68e213de78ee5f1fadbdc8930e9898516
Marcell Sárkány <marci.sarkany@gmail.com>
2022-02-18 12:46:23 +0100

    Add RestService skeleton

---
+++ client/src/app/rest.service.ts
@@ -0,0 +1,62 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class RestService {
+
+    private url: string = '';
+    private readonly http: HttpClient;
+
+    constructor(http: HttpClient) {
+        this.http = http;
+    }
+
+    // 'item' parameter is only for the prototype
+    public async get<T>(endpoint: string, item: T): Promise<T> {
+        await new Promise(r => setTimeout(r, 1000));
+        return item;
+
+        // const result = await firstValueFrom(this.http.get<T>(this.url + endpoint, {
+        //     headers: { 'username': 'username', 'token': 'token' }
+        // }));
+
+        // return result;
+    }
+
+    public async post<T>(endpoint: string, item: T): Promise<T> {
+        await new Promise(r => setTimeout(r, 1000));
+        return item;
+
+        // const result = await firstValueFrom(this.http.post<T>(this.url + endpoint, item, {
+        //     headers: { 'username': 'username', 'token': 'token' }
+        // }));
+
+        // return result;
+    }
+
+    public async put<T>(endpoint: string, item: T): Promise<T> {
+        await new Promise(r => setTimeout(r, 1000));
+        return item;
+
+        // const result = await firstValueFrom(this.http.put<T>(this.url + endpoint, item, {
+        //     headers: { 'username': 'username', 'token': 'token' }
+        // }));
+
+        // return result;
+    }
+
+    // 'item' parameter is only for the prototype
+    public async delete<T>(endpoint: string, item: T): Promise<T> {
+        await new Promise(r => setTimeout(r, 1000));
+        return item;
+
+        // const result = await firstValueFrom(this.http.delete<T>(this.url + endpoint, {
+        //     headers: { 'username': 'username', 'token': 'token' }
+        // }));
+
+        // return result;
+    }
+}
