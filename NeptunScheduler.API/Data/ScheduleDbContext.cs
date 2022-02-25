adddb13ceb67d4e1d2330cb6eef206f458f5e315
Marcell Sárkány <marci.sarkany@gmail.com>
2022-02-25 11:16:31 +0100

    Add User model to the context

--- NeptunScheduler.API/Data/ScheduleDbContext.cs
+++ NeptunScheduler.API/Data/ScheduleDbContext.cs
@@ -5,11 +5,7 @@ namespace NeptunScheduler.Data
 {
     public class ScheduleDbContext : DbContext
     {
-        public DbSet<Subject> Subjects { get; set; }
-
-        public DbSet<Course> Courses { get; set; }
-
-        public DbSet<BusyTimeBlock> BusyTimeBlocks { get; set; }
+        public DbSet<User> Users { get; set; }
 
         public ScheduleDbContext(DbContextOptions<ScheduleDbContext> options)
             : base(options)
