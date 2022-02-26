54ba2c66eb81423fac8beb8ec991636c7946875d
Marcell Sárkány <marci.sarkany@gmail.com>
2022-02-26 13:19:23 +0100

    Remove simulated result in CoursesComponent

--- client/src/app/courses/courses.component.ts
+++ client/src/app/courses/courses.component.ts
@@ -44,33 +44,6 @@ export class CoursesComponent extends CrudComponent<CourseVm, CourseDto> impleme
     }
 
     public processGetResult(res: Array<CourseDto>): void {
-        let c1 = new CourseDto();
-        c1.id = 'id1';
-        c1.code = 'ABC-123';
-        c1.slots = 4;
-        c1.day = 1;
-        c1.start = 480;
-        c1.end = 580;
-        c1.teachers = 'Valaki Sanyi';
-        c1.fix = true;
-        c1.collidable = false;
-        c1.ignored = false;
-        c1.priority = 5;
-
-        let c2 = new CourseDto();
-        c2.id = 'id2';
-        c2.code = 'XZY-456';
-        c2.slots = 2;
-        c2.day = 2;
-        c2.start = 630;
-        c2.end = 720;
-        c2.teachers = 'Simon Péter';
-        c2.fix = false;
-        c2.collidable = true;
-        c2.ignored = true;
-        c2.priority = 10;
-
-        res.push(c1, c2);
         this.models = res.map(c => Object.assign(new CourseDto(), c).toVm());
     }
 
