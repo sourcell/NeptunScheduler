b7c6aba42d0116c469f3e5bc46e9b874cb5ec30b
Marcell Sárkány <marci.sarkany@gmail.com>
2022-03-02 22:08:21 +0100

    Add IBusyTimeblockRepository

---
+++ NeptunScheduler.Repository/IBusyTimeblockRepository.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Linq;
+using NeptunScheduler.Models;
+
+namespace NeptunScheduler.Repository
+{
+    public interface IBusyTimeblockRepository : IRepository<BusyTimeblock>
+    {
+    }
+}
